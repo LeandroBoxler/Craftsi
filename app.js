898e5abc0e510024667135ca3ed97175327a30dd
LeandroBoxler <leandroboxler@gmail.com>
2023-11-03 21:07:56 -0300

    login y register hechos

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require("path")
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"views","home.html")))
+app.get("/register",(req,res)=>res.sendFile(path.join(__dirname,"views","register.html")))
+app.get("/login",(req,res)=>res.sendFile(path.join(__dirname,"views","login.html")))
+app.get("/login",(req,res)=>res.sendFile(path.join(__dirname,"views","profile.html")))
+
 
 
 app.listen(3000,()=>console.log("vamo los pibes"))
